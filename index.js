ae8110eb8bb785a21b425052c37beed0ce4083a3
JoanGoAl <joangonzalezalbert@gmail.com>
2023-01-08 22:28:27 +0100

    bot telegram prueba 1

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+// bot name -> telegramJoan
+// username -> telejoanbot
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TELEGRAM_TOKEN;
+
+const bot = new TelegramBot(token, {polling: true});
+
+// Message when the bot is started
+bot.onText(/\/start/, (msg) => {
+    const chatId = process.env.TELEGRAM_CHAT_ID
+    bot.sendMessage(chatId, 'Workflow ejecutado correctamente tras el último commit. Saludos Joan González. 🤖');
+});
+
+
+bot.on('message', (msg) => {
+    const chatId = process.env.TELEGRAM_CHAT_ID ;
+    bot.sendMessage(chatId, 'Mensaje enviado');
+});
+
+
 app.get('/', (req, res) => {
     res.send('Hello World! 🤌')
 })
